34898a9c1d378b69386275af1ed266aa5a844710
Yuri <dr.dampel@yandex.ru>
2023-03-26 10:14:20 +0300

    Mantine library added. Btn signIn added to header.

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import s from './Header.module.css';
+import { Button } from '@mantine/core';
 
 export const Header = () => {
     return(
         <div className={s.outterContainerHeader}>
             <div className={s.logo}>Active Book</div>
-            <div className={s.controls}>Войти</div>
+            <div className={s.controls}>
+                <Button>Войти</Button>
+            </div>
         </div>
     )
 }
