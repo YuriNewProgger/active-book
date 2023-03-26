0718297df34af9663417989142e6f5571c391f9b
Yuri <dr.dampel@yandex.ru>
2023-03-26 20:34:54 +0300

    Component projects bar added. Style added for project bar.

--- src/Components/Body/Body.tsx
+++ src/Components/Body/Body.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import s from './Body.module.css';
+import { ProjectsBar } from "./ProjectsBar/ProjectsBar";
 
 export const Body = () => {
     return(
         <div className={s.outterContainerBody}>
-            Body
+                <ProjectsBar/>
+                Tasks
         </div>
     )
 }
---
+++ src/Components/Body/ProjectsBar/ProjectsBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './ProjectsBar.module.css';
+
+
+export const ProjectsBar = () => {
+    return(
+        <div className={s.outterContainerProjectsBar}>
+            Project bar
+        </div>
+    )
+}
