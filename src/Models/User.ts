794b1838b6418e6dbc8ff956f486d0b976f22ae5
Yuri <dr.dampel@yandex.ru>
2023-03-26 13:03:52 +0300

    Model project added. Model task added.

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -1,9 +1,15 @@
+import { Task } from './Task';
+import { Project } from './Project';
+
 export class User{
+
     constructor(
         public name: string, 
         public surName: string, 
         public patronymic: string,
         public position: string,
-        public role: string)
+        public role: string,
+        public projects: Project[],
+        public tasks: Task[])
         {}
 }
